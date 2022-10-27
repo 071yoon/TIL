dfb0eb3986d6fef3b4ca7e2b3d584cb5eb0ee17b
071yoon <yeonggilol@gmail.com>
2022-10-27 17:29:45 +0900

    mediator pattern java impl

---
+++ Design Pattern/Mediator Pattern/MediatorPatternImpl/src/ColleagueCheckbox.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class ColleagueCheckbox extends Checkbox implements ItemListener, Colleague {
+    private Mediator mediator;
+
+    public ColleagueCheckbox(String label, CheckboxGroup group, boolean state) throws HeadlessException {
+        super(label, group, state);
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        setEnabled(enabled);
+    }
+
+    public void itemStateChanged(ItemEvent event) {
+        mediator.colleagueChanged();
+    }
+}
---
+++ Design Pattern/Mediator Pattern/MediatorPatternImpl/src/ColleagueTextField.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+
+public class ColleagueTextField extends TextField implements TextListener, Colleague {
+    private Mediator mediator;
+
+    public ColleagueTextField(String text, int columns) throws HeadlessException {
+        super(text, columns);
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        setEnabled(enabled);
+        setBackground(enabled?Color.white : Color.lightGray);
+    }
+
+    public void textValueChanged(TextEvent event) {
+        mediator.colleagueChanged();
+    }
+}
